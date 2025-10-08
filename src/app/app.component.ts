c602c969d990e92a2ff5823139e48dad564d0d63
BibinDavis <bibin.davis@gmail.com>
2025-10-08 14:53:15 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { ReactiveFormsModule, FormGroup } from '@angular/forms';
 
+import { RouterOutlet } from "@angular/router";
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink],
+  imports: [ReactiveFormsModule, RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush //Change Detection Functionality
